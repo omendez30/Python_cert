ad03c0c27d32ec1d2d0e530187e166b8708a328d
Orangel Mendez <omendez30@users.noreply.github.com>
2022-04-24 12:06:25 -0400

    Adding comments and color

--- Nasa/nasa.py
+++ Nasa/nasa.py
@@ -1,44 +1,61 @@
 #!/usr/bin/env python3
 
 #import libraries
+
 import requests
 import shutil
+import crayons
+
 
 #set the Astronomy picture of the day API key to a constant
-APOD_API = "https://api.nasa.gov/planetary/apod" 
+APOD_API = "https://api.nasa.gov/planetary/apod"
 
+#This function will download the photo of the day to you current directory
 
 def img_download(url):
-	file_name = input("Save image as (string): ")
-	r = requests.get(url, stream=True)
+	see_photo = input("Would you like to see today Astronomy photo of the day? Y/N: ").lower()
 	
-	if r.status_code == 200:
-		with open(file_name, 'wb') as file:
-			shutil.copyfileobj(r.raw,file)
-		print("Image successfully downloaded: ", file_name)
-		print("Please check your repo directory to see todays image")
+	if see_photo == 'y':
+		file_name = input("Save image as (string): ")
+		
+		#retrieves the image using get, stream ensures there's no interruption while method is running
+		r = requests.get(url, stream=True)
+	
+		if r.status_code == 200: #if response is successful, and returns 200
+			with open(file_name, 'wb') as file: #open the file using writ-and-binary mode
+				shutil.copyfileobj(r.raw,file) #copying the content of the file like object to the file
+			print("Image successfully downloaded: ", file_name) #img downloaded, and saved into the file name that was given
+			print("Please check your repos directory under the submitted name to see todays image")
+		else:
+			print("Image not found")
 	else:
-		print("Image not found")
+		print("Thanks for checking out NASA's Astronomy Photo of the day")
+		return
 
 def main():
 
 	#Send HTTPS Get to the API of Nasa's astronomy picture of the day.
 	get_apod_info = requests.get(f'{APOD_API}?api_key=DEMO_KEY')
+
+	#Generated an API key in the event that I surpassed the rate limit allowed
+	#get_apod_info = requests.get(f'{APOD_API}?api_key=c5S9CpJiWOhefmhFxa7OVuVi3zcWGTP0WYoh0b03')
 	
 	#decodes response
 	got_apod = get_apod_info.json()
 	
-	url = got_apod['url']
-	title = got_apod['title']
-	date = got_apod['date']
-	explanation = got_apod['explanation']
+	
+	url = got_apod['url'] #get the url used for the photo of the day	
+	title = got_apod['title'] #title of the photo
+	date = got_apod['date'] #"date published
+	explanation = got_apod['explanation'] #Explaination of the photo
+	
+	#print the info above highlighting the titles of each section in green
+	#print(f"{got_apod}\n\n")	
+	print(crayons.green("Welcome to NASA's ASTRONOMY PHOTO OF THE DAY!!\n", bold=True))
+	print(f"{crayons.green('Title:')} {title}\n")
+	print(f"{crayons.green('Date:')} {date}\n")
+	print(f"{crayons.green('Explanation:')}  {explanation}\n")
 
-	print(f"Title: {title}\n")
-	print(f"Date:{date}\n")
-	print(f"Explanation:  {explanation}\n")
-
-	#for apod in got_apod:
-		#print(f"\n{apod}:\n{got_apod[apod]}\n")
 	
 	img_download(url)
 		
--- README.md
+++ README.md
@@ -10,6 +10,9 @@ This code is fulfill requirements set by Alta3 research for python basics certif
 
 Ensure that you have python package manager pip installed using, sudo apt install python3-pip,for Mac. Also import shutil, and requests modules. Windows, download the get-pip.py open command prompt and navigate to the get-pip.py installer, and run the command python get-pip.py. 
 
+##API KEY
+
+Nasa's API site provides a DEMO KEY which will grant access to nasa's API. These keys allows 30 requests per IP address per hour, and 50 requests per IP address per day. In the event that you reach this limit, please generate an API key.
 
 ## Authors
 
